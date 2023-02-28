ad27a87a83a50e09415a587c0af249aba01982db
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-27 22:25:53 -0300

    aplicando um tipo que guardará os valores das cores

--- src/components/Button.styled.ts
+++ src/components/Button.styled.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
-interface ButtonContainerProps  {
-    variant?: "primary" | "secondary" | "danger" | "neutral";
+
+type ButtonVariant = "primary" | "secondary" | "danger" | "neutral"; //criando tipos para passar a cor no variant
+interface ButtonContainerProps {
+  variant: ButtonVariant;
 }
-export const ButtonContainer =  styled.button`
-width:100px;
- height: 40px
- `
+export const ButtonContainer = styled.button`
+  width: 100px;
+  height: 40px;
+`;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 
-import { ButtonContainer } from "./Button.styled";
+import { ButtonContainer } from "./Button.styled"; //import do styles
 
 
 interface ButtonProps {
-  variant?: "primary" | "secondary" | "danger" | "neutral";
+  variant?: "primary" | "secondary" | "danger" | "neutral";  //interface que recebe os valores das cores
 }
-export function Button({ variant = "primary" }: ButtonProps) {
+export function Button({ variant = "primary" }: ButtonProps) { //colocamos variant como primary para caso nao
+  //tenha cor definida ele passe como padrao a primaria
   return (
     <>
       <ButtonContainer variant={color}>Enviar</ButtonContainer>
