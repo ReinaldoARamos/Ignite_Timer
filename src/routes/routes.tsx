425328ae179653e657a75cdbcbe152ceec72057f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-01 11:24:16 -0300

    criando o padrao layout e aplicando Header em todas as rotas usando outlet

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,15 @@ import {Routes, Route} from 'react-router-dom';
 
 import { Home  } from '../pages/home'
 import { History } from '../pages/history'
+import { DefaultLayout } from '../layouts/Default';
 
 export function Router(){
     return(
      <Routes>
+        <Route path='/' element={<DefaultLayout/>}> 
         <Route path="/" element={<Home/>}/>
         <Route path="/history" element={<History/>}/>
+        </Route>
      </Routes>
     )
 }
