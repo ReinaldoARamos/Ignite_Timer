d7021cf6506193a36600560ead47d035fe0c7c08
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-01 11:17:36 -0300

    criando um meio de usar o HEader em todos os locais da aplicação através do Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-// import { useState } from 'react'
-import { Button } from './components/Button'
+import { useState } from 'react'
+
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
-import { Router } from './routes/routes'
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes/routes' //importação de rotas
+import { BrowserRouter } from 'react-router-dom' //temos que esperifdicar as rotas do browser, fazer um wrap ali no return
 
 export function App() {
   // const [count, setCount] = useState(0)
