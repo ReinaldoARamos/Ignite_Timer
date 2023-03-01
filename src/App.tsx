d8ca146405321ad519d78ad12ac23e4d48b7f18f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-02-28 22:34:44 -0300

    configurando o eslint para padronizar o código

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  //passando o themeProvider nos botoes
+  // passando o themeProvider nos botoes
   return (
-    <ThemeProvider theme={defaultTheme}> 
-      <Button variant='primary'/>
-      <Button variant='neutral'/>
-      <Button variant='secondary'/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="neutral" />
+      <Button variant="secondary" />
       <Button />
 
-      <GlobalStyle/>
-      
+      <GlobalStyle />
     </ThemeProvider>
-    )
+  )
 }
-
-
