234c8222c46b493c0251c75b4559cf6857275301
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-02 17:10:38 -0300

    Arrumando as pastas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from '../pages/Home/index'
-//import { History } from '../pages/Home'
+import { History } from '../pages//History/index'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
