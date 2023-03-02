b4c6085b4ddad24e62478fcf04d09c003d3a84ce
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-01 22:49:28 -0300

    criando uma cor para caso o link esteja ativo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
 import { HeaderContainer } from './styles'
 import { Timer, Scroll } from 'phosphor-react'
 import Logo from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
 export function Header() {
   return (
     <>
       <HeaderContainer>
         <img src={Logo} alt="" srcSet="" />
         <nav>
-          <a href="/">
-            <Timer />
-          </a>
+          <NavLink to="/">
+            <Timer size={24} />
+          </NavLink>
 
-          <a href="#">
-            <Scroll />
-          </a>
+          <NavLink to="/history">
+            <Scroll size={24} />
+          </NavLink>
         </nav>
       </HeaderContainer>
     </>
