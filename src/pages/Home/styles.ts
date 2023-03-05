6af3d2fbf25b975c30d7783e0b3ea9e2fdc4b122
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-05 16:44:59 -0300

    criando a estilização dos campos

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,24 +1,31 @@
-import styled from "styled-components";
-
+import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
-flex: 1;
-display: flex;
-flex-direction: column;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 
-align-items: center;
-justify-content: center;
+  align-items: center;
+  justify-content: center;
 
-form{
+  form {
     display: flex;
     align-items: center;
     flex-direction: column;
     gap: 3.5rem;
-}
+  }
 `
 
 export const FormContainer = styled.form`
-` 
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap; //Caso a tela dimunua ele quebra os campos em mais linhas
+`
 
-export const CountdownContainer = styled.div`
-` 
+export const CountdownContainer = styled.div``
