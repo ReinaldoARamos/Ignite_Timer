8bb859fea749fb0a51384c9082ef37418f039151
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-06 17:39:54 -0300

    criando a base das tabelas

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,30 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
-  return <div>Hello History</div>
+  return(
+  <HistoryContainer>
+    <h1>Meu histórico</h1>
+
+    <HistoryList>
+      <table>
+        <thead>
+          <tr>
+            <th>Terefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td>Concluído</td>
+          </tr>
+        </tbody>
+      </table>
+    </HistoryList>
+  </HistoryContainer>
+  )
 }
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,1 +1,8 @@
 import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+flex: 1;
+padding: 3.5rem
+`
+
+export const HistoryList = styled.div``
