947a4af7938bac06b71c4f3a67d121a4d3f2bfb0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-09 17:37:53 -0300

    criando um estado que irá armazenar os valores dos ciclos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Play, Watch } from 'phosphor-react'
-import { useState } from 'react'
-import* as zod from  'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
+import { Play, Watch } from "phosphor-react";
+import { useState } from "react";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   CountdownContainer,
   FormContainer,
@@ -10,40 +10,49 @@ import {
   Separator,
   StartCountDownButton,
   TaskInput,
-} from '../Home/styles'
-import { useForm } from 'react-hook-form'
-import { Action } from '@remix-run/router'
+} from "../Home/styles";
+import { useForm } from "react-hook-form";
+import { Action } from "@remix-run/router";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod.number().min(5).max(60),
-})
+});
 /*
 O Register é uma função que vem junto do hook form, atravé dela temos diversos acessos a outros métodos
 que são geralmente usado com funções, como onchange, onblue e até onFocus */
-export function Home() {
-  const { register, handleSubmit, watch, formState, reset } = useForm<newCycleData>({
-    resolver: zodResolver(newCycleFormValidationSchema), 
-    defaultValues: {
-      task: '',
-      minutesAmount: 0
-    } 
-  })
 
-  type newCycleData = zod.infer<typeof newCycleFormValidationSchema>
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmout: number;
+} //criando interface dos ciclos
+
+export function Home() {
+  const [cycles, SetCycles] = useState<Cycle[]>([]); //iniciando um estado que irá armazenar todos os ciclos
+  const { register, handleSubmit, watch, formState, reset } =
+    useForm<newCycleData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: "",
+        minutesAmount: 0,
+      },
+    });
+
+  type newCycleData = zod.infer<typeof newCycleFormValidationSchema>;
 
   function handleCreateNewCycle(data: newCycleData) {
-    console.log(data)
+    console.log(data);
     reset();
   }
-console.log(formState.errors)
+  console.log(formState.errors);
 
-  const task = watch('task')
+  const task = watch("task");
 
   return (
     //aqui no handleSubmit o HandleCreate new Cyle que pega os dados no Onsubmit
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCycle)} action=""> 
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <div>
             <label htmlFor="task">Vou trabalher em</label>
@@ -51,7 +60,7 @@ console.log(formState.errors)
               id="task"
               list="task-suggestgions"
               placeholder="Definir tarefa  "
-              {...register('task')}
+              {...register("task")}
               // usando o ... a gente retorna todos os métodos dentro do register
             />
             <datalist id="task-suggestgions">
@@ -67,8 +76,7 @@ console.log(formState.errors)
               placeholder="00"
               min={5}
               step={5}
-              
-              {...register('minutesAmount', { valueAsNumber: true })}
+              {...register("minutesAmount", { valueAsNumber: true })}
             />
 
             <span>minutos.</span>
@@ -89,5 +97,5 @@ console.log(formState.errors)
         </StartCountDownButton>
       </form>
     </HomeContainer>
-  )
+  );
 }
