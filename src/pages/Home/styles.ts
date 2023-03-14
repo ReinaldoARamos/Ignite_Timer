867907f123c66d3686c076354130b9ba2375e621
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-14 10:01:40 -0300

    adicionando uma transição ´para ficar mais smooth quando ficar disabled no input

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -96,14 +96,19 @@ const BaseInput = styled.input`
   height: 2.5rem;
   font-size: 1.125rem;
   padding: 0 0.5rem;
+  transition: 0.4s;
+
   &:disabled {
     border-color: ${(props) => props.theme['gray-700']};
     cursor: not-allowed;
     color: ${(props) => props.theme['gray-700']};
+
   }
 
   &:placeholder {
     color: ${(props) => props.theme['gray-500']};
+    
+
   }
 
   &:focus {
