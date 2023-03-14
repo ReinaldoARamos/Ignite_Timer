96500c4d55d6772e178cf08ba6f3115fc6fc5e0d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-14 09:44:43 -0300

    adicionando uma condicional dentro do useEffect aplicando uma lógica semelhante ao do InterruptCycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
-import { HandPalm, Play, Watch } from 'phosphor-react'
-import { useEffect, useState } from 'react'
-import * as zod from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { HandPalm, Play, Watch } from "phosphor-react";
+import { useEffect, useState } from "react";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   CountdownContainer,
   FormContainer,
@@ -11,75 +11,89 @@ import {
   StartCountDownButton,
   StopCountDownButton,
   TaskInput,
-} from '../Home/styles'
-import { useForm } from 'react-hook-form'
-import { Action } from '@remix-run/router'
-import { differenceInSeconds } from 'date-fns'
+} from "../Home/styles";
+import { useForm } from "react-hook-form";
+import { Action } from "@remix-run/router";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
+  task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod.number().min(0.5).max(60),
-})
+});
 /*
 O Register é uma função que vem junto do hook form, atravé dela temos diversos acessos a outros métodos
 que são geralmente usado com funções, como onchange, onblue e até onFocus */
 
 interface Cycle {
-  id: string
-  task: string
-  minutesAmout: number
-  startDate: Date
-  interruptedDate?: Date
+  id: string;
+  task: string;
+  minutesAmout: number;
+  startDate: Date;
+  interruptedDate?: Date;
 } // criando interface dos ciclos
 
 export function Home() {
-  const [cycles, SetCycles] = useState<Cycle[]>([]) // iniciando um estado que irá armazenar todos os ciclos
-  const [activeCycleId, SetActiveCycle] = useState<string | null>(null) // inicisndo o estado que vai verificar o estado ativo
-  const [amountSecondsPass, setamountSecondsPass] = useState(0)
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  const [cycles, SetCycles] = useState<Cycle[]>([]); // iniciando um estado que irá armazenar todos os ciclos
+  const [activeCycleId, SetActiveCycle] = useState<string | null>(null); // inicisndo o estado que vai verificar o estado ativo
+  const [amountSecondsPass, setamountSecondsPass] = useState(0);
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  const TotalSeconds = activeCycle ? activeCycle.minutesAmout * 60 : 0; // ´pega os minutos passados e retorna sem segundos
 
   const { register, handleSubmit, watch, formState, reset } =
     useForm<newCycleData>({
       resolver: zodResolver(newCycleFormValidationSchema),
       defaultValues: {
-        task: '',
+        task: "",
         minutesAmount: 0,
       },
-    })
+    });
 
   useEffect(() => {
-    let interval: number
+    let interval: number;
     if (activeCycle) {
       interval = setInterval(() => {
-        setamountSecondsPass(
-          differenceInSeconds(new Date(), activeCycle.startDate),
-        )
-      }, 1000)
+        const SecondsDiff = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
+        );
+        if (SecondsDiff >= TotalSeconds) {
+          SetCycles(
+            cycles.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, interruptedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          );
+          SetActiveCycle(null);
+        }
+        setamountSecondsPass(SecondsDiff);
+      }, 1000);
     }
     return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle])
+      clearInterval(interval);
+    };
+  }, [activeCycle, TotalSeconds]);
 
-  type newCycleData = zod.infer<typeof newCycleFormValidationSchema>
-  const id = String(new Date().getTime())
+  type newCycleData = zod.infer<typeof newCycleFormValidationSchema>;
+  const id = String(new Date().getTime());
 
-  const TotalSeconds = activeCycle ? activeCycle.minutesAmout * 60 : 0 // ´pega os minutos passados e retorna sem segundos
-  const currentSeconds = activeCycle ? TotalSeconds - amountSecondsPass : 0 // pega os totais de segundos e diminui pelo que passa
+  const currentSeconds = activeCycle ? TotalSeconds - amountSecondsPass : 0; // pega os totais de segundos e diminui pelo que passa
 
-  const minutesAmount = Math.floor(currentSeconds / 60) // PEGA OS SEGUNDOS e converte em minutos
-  const secondsAmout = currentSeconds % 60 // pega o resto da divisao
+  const minutesAmount = Math.floor(currentSeconds / 60); // PEGA OS SEGUNDOS e converte em minutos
+  const secondsAmout = currentSeconds % 60; // pega o resto da divisao
 
-  const minutes = String(minutesAmount).padStart(2, '0')
-  const seconds = String(secondsAmout).padStart(2, '0')
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmout).padStart(2, "0");
 
   useEffect(() => {
     if (activeCycle) {
-      document.title = `${minutes} : ${seconds}`
+      document.title = `${minutes} : ${seconds}`;
     }
-  }, [minutes, seconds, activeCycle])
+  }, [minutes, seconds, activeCycle]);
 
-  console.log(cycles)
+  console.log(cycles);
 
   function handleCreateNewCycle(data: newCycleData) {
     const newCycle: Cycle = {
@@ -87,28 +101,28 @@ export function Home() {
       task: data.task,
       minutesAmout: data.minutesAmount,
       startDate: new Date(),
-    }
+    };
 
-    SetCycles((state) => [...state, newCycle]) // adicionando um estado novo pegando o anterior e passando o novo
-    SetActiveCycle(id)
-    setamountSecondsPass(0)
-    reset()
+    SetCycles((state) => [...state, newCycle]); // adicionando um estado novo pegando o anterior e passando o novo
+    SetActiveCycle(id);
+    setamountSecondsPass(0);
+    reset();
   }
 
   function HandleInterruptedCycle() {
     SetCycles(
       cycles.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
+          return { ...cycle, interruptedDate: new Date() };
         } else {
-          return cycle
+          return cycle;
         }
-      }),
-    )
-    SetActiveCycle(null)
+      })
+    );
+    SetActiveCycle(null);
   }
 
-  const task = watch('task')
+  const task = watch("task");
 
   return (
     // aqui no handleSubmit o HandleCreate new Cyle que pega os dados no Onsubmit
@@ -122,7 +136,7 @@ export function Home() {
               list="task-suggestgions"
               placeholder="Definir tarefa  "
               disabled={!!activeCycle}
-              {...register('task')}
+              {...register("task")}
               // usando o ... a gente retorna todos os métodos dentro do register
             />
             <datalist id="task-suggestgions">
@@ -139,7 +153,7 @@ export function Home() {
               min={5}
               step={5}
               disabled={!!activeCycle}
-              {...register('minutesAmount', { valueAsNumber: true })} // register
+              {...register("minutesAmount", { valueAsNumber: true })} // register
             />
 
             <span>minutos.</span>
@@ -167,5 +181,5 @@ export function Home() {
         )}
       </form>
     </HomeContainer>
-  )
+  );
 }
