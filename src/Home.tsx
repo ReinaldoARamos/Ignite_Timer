0ebb2f9a96d3cd4862fc889e55fb327d494813eb
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-15 15:39:15 -0300

    setando o estado do activeCycle e passando dentro do context provider, tornando acessável para outros componentes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,26 +1,36 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { any } from "zod";
 
-const CyclesContext = createContext({
-  activeCycle: "ohayo"
-});
+const CyclesContext = createContext({} as any);
 function NewCycleForm() {
-    let { activeCycle } = useContext(CyclesContext);
-  
-    return <h1>newCycleForm: {activeCycle}</h1>;
-  }
+  let { activeCycle } = useContext(CyclesContext);
 
+  return (
+    <div>
+      <h1>newCycleForm: {activeCycle}</h1>
+      <button  onClick={()=>{
+        activeCycle = 2
+      }}>
+        Alterar ciclo
+    </button>
+    </div>
+  );
+}
 function Coundown() {
   let { activeCycle } = useContext(CyclesContext);
   return <h1>newCycleForm: {activeCycle}</h1>;
 }
 
-
 export function Home() {
+  const [activeCycle, setActiveCycle] = useState(0);
+
   return (
-    <div>
-      [<h1>Home</h1>]
-      <NewCycleForm />
-      <Coundown/>
-    </div>
+    <CyclesContext.Provider value={{activeCycle, setActiveCycle}}>
+      <div>
+        [<h1>Home</h1>]
+        <NewCycleForm />
+        <Coundown />
+      </div>
+    </CyclesContext.Provider>
   );
 }
