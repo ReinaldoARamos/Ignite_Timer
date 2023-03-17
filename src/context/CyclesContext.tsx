0c26414a7bd9c1a0445a2e981b2e95a57e4fcef5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-16 22:05:31 -0300

    criando uma interface com os dados do createnewCycle, tipando e chamando no context

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -1,100 +1,109 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-//---------------------------------------------
+// ---------------------------------------------
 
-interface CyclesContextData {
-  activeCycle: Cycle | undefined; // passando a interface do contexto como a interface cycle
-  activeCycleId: string | null;
-  markCurrentCycleAsFinished: () => void;
-  amountSecondsPass: number;
-  setSecondsPass: (seconds: number) => void;
+interface CreateCycleData {
+  // dados pra passar no createNewCycle
+  task: string
+  minutesAmount: number
 }
 
-//---------------------------------------------
+// ---------------------------------------------
+
+interface CyclesContextData {
+  activeCycle: Cycle | undefined // passando a interface do contexto como a interface cycle
+  activeCycleId: string | null
+  markCurrentCycleAsFinished: () => void
+  amountSecondsPass: number
+  setSecondsPass: (seconds: number) => void
+  CreateNewCycle: (data: CreateCycleData) => void
+  InterruptedCycle: () => void
+}
+
+// ---------------------------------------------
 
 interface Cycle {
-  id: string;
-  task: string;
-  minutesAmout: number;
-  startDate: Date;
-  interruptedDate?: Date;
-  FinishedDate?: Date;
-  amountSecondsPass: number;
-  
+  id: string
+  task: string
+  minutesAmout: number
+  startDate: Date
+  interruptedDate?: Date
+  FinishedDate?: Date
+  amountSecondsPass: number
 } // criando interface dos ciclos
 
-//---------------------------------------------
+// ---------------------------------------------
 
-export const CyclesContext = createContext({} as CyclesContextData); // criando o contexto do ciclo
+export const CyclesContext = createContext({} as CyclesContextData) // criando o contexto do ciclo
 
 export function CyclesContextProvider() {
-  const [cycles, SetCycles] = useState<Cycle[]>([]); // iniciando um estado que irá armazenar todos os ciclos
-  const [activeCycleId, SetActiveCycle] = useState<string | null>(null); // inicisndo o estado que vai verificar o estado ativo
-  const [amountSecondsPass, setamountSecondsPass] = useState(0);
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  const [cycles, SetCycles] = useState<Cycle[]>([]) // iniciando um estado que irá armazenar todos os ciclos
+  const [activeCycleId, SetActiveCycle] = useState<string | null>(null) // inicisndo o estado que vai verificar o estado ativo
+  const [amountSecondsPass, setamountSecondsPass] = useState(0)
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
-  const id = String(new Date().getTime());
+  const id = String(new Date().getTime())
 
   function setSecondsPass(seconds: number) {
-    setamountSecondsPass(seconds);
+    setamountSecondsPass(seconds)
   }
-
 
   function markCurrentCycleAsFinished() {
     SetCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          document.title = "Home";
-          return { ...cycle, FinishedDate: new Date() };
+          document.title = 'Home'
+          return { ...cycle, FinishedDate: new Date() }
         } else {
-          return cycle;
+          return cycle
         }
-      })
-    );
+      }),
+    )
   }
 
-  //---------------------------------------------
-  function CreateNewCycle(data: newCycleData) {
+  // ---------------------------------------------
+  function CreateNewCycle(data: CreateCycleData) {
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmout: data.minutesAmount,
       startDate: new Date(),
-      amountSecondsPass: amountSecondsPass,
-    };
+      amountSecondsPass,
+    }
 
-    SetCycles((state) => [...state, newCycle]); // adicionando um estado novo pegando o anterior e passando o novo
-    SetActiveCycle(id);
-    setamountSecondsPass(0);
-    reset();
+    SetCycles((state) => [...state, newCycle]) // adicionando um estado novo pegando o anterior e passando o novo
+    SetActiveCycle(id)
+    setamountSecondsPass(0)
+    //  reset()
   }
 
-  //---------------------------------------------
+  // ---------------------------------------------
 
   function InterruptedCycle() {
     SetCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          document.title = "Home";
-          return { ...cycle, interruptedDate: new Date() };
+          document.title = 'Home'
+          return { ...cycle, interruptedDate: new Date() }
         } else {
-          return cycle;
+          return cycle
         }
-      })
-    );
-    SetActiveCycle(null);
+      }),
+    )
+    SetActiveCycle(null)
   }
 
-
-  <CyclesContext.Provider
+  ;<CyclesContext.Provider
     value={{
       activeCycle,
       activeCycleId,
       markCurrentCycleAsFinished,
       amountSecondsPass,
       setSecondsPass,
+      CreateNewCycle,
+      InterruptedCycle,
     }}
   >
     -
-  </CyclesContext.Provider>;
+  </CyclesContext.Provider>
 }
