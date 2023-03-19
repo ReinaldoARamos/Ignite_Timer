fac9118007490d2fb18243fa037be5be88a73191
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-19 20:07:35 -0300

    arrumanedo o erro de date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,10 +1,10 @@
-import { useContext } from "react";
-import { CyclesContext } from "../../context/CyclesContext";
-import { HistoryContainer, HistoryList, StatusColor } from "./styles";
-import {formatDistanceToNow} from 'date-fns';
-import ptBR  from "date-fns/locale/pt-BR";
+import { useContext } from 'react'
+import { CyclesContext } from '../../context/CyclesContext'
+import { HistoryContainer, HistoryList, StatusColor } from './styles'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 export function History() {
-  const { cycles } = useContext(CyclesContext);
+  const { cycles } = useContext(CyclesContext)
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -25,31 +25,33 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmout}</td>
-                  <td>{formatDistanceToNow(cycle.startDate, {
-                    addSuffix: true,
-                    locale: ptBR
-                  })}</td>
                   <td>
-                   {cycle.FinishedDate && (
-                   <StatusColor StatusColor="green">Concluído</StatusColor>)}
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.FinishedDate && (
+                      <StatusColor StatusColor="green">Concluído</StatusColor>
+                    )}
 
                     {cycle.interruptedDate && (
-                   <StatusColor StatusColor="red">Interrompido</StatusColor>)}
-                  
-                    
-                  {(!cycle.FinishedDate && !cycle.interruptedDate) && (
-                   <StatusColor StatusColor="yellow">Em Andamento</StatusColor>)}
-                  </td>
-                
-                  
+                      <StatusColor StatusColor="red">Interrompido</StatusColor>
+                    )}
 
-                  
+                    {!cycle.FinishedDate && !cycle.interruptedDate && (
+                      <StatusColor StatusColor="yellow">
+                        Em Andamento
+                      </StatusColor>
+                    )}
+                  </td>
                 </tr>
-              );
+              )
             })}
           </tbody>
         </table>
       </HistoryList>
     </HistoryContainer>
-  );
+  )
 }
